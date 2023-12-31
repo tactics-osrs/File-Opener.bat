75b95a3ab85eec87ee90a06041a8b0d00fd15c8d
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2023-12-31 18:37:47 -0500

    Create Folder Opener.bat

---
+++ Folder Opener.bat
@@ -0,0 +1,20 @@
+@echo off
+:Start
+set /p FolderName="Enter the folder path: "
+if exist "%FolderName%" (
+    icacls "%FolderName%" >nul 2>&1
+    if errorlevel 1 (
+        echo You need elevated privileges to open this folder.
+        pause
+        goto Start
+    ) else (
+        start "" "%FolderName%"
+        echo The folder "%FolderName%" was successfully opened.
+        pause
+        goto Start
+    )
+) else (
+    echo The folder does not exist.
+    pause
+    goto Start
+)
